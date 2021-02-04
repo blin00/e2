6e4f705ad9925385d583579bd2b62a3a41f2c862
Brandon Lin <blin00@gmail.com>
2021-02-03 18:46:23 -0800

    random urls

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,5 +4,6 @@ import { render } from 'react-dom';
 import App from './App';
 
 fetch('config.json').then(resp => resp.json()).then(data => {
-    render(<App videoUrl={data.url} skew={data.skew} />, document.getElementById("root"));
+    let idx = Math.floor(Math.random() * data.urls.length);
+    render(<App videoUrl={data.urls[idx]} skew={data.skew} />, document.getElementById("root"));
 });
